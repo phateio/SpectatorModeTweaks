f86c0637e560c2ee388a41946ebf84a486a0f662
WhiteCat <whitecatx6@gmail.com>
2022-02-06 23:14:29 +0800

    Add new option: gameModeSurvivalWhenJoin

--- src/main/java/io/github/phateio/spectatormodetweaks/SpectatorModeTweaks.java
+++ src/main/java/io/github/phateio/spectatormodetweaks/SpectatorModeTweaks.java
@@ -1,17 +1,30 @@
 package io.github.phateio.spectatormodetweaks;
 
 import org.bukkit.GameMode;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SpectatorModeTweaks extends JavaPlugin implements Listener {
 
+    static final String path_gameModeSurvivalWhenJoin = "gameModeSurvivalWhenJoin";
+    private boolean gameModeSurvivalWhenJoin = false;
+
     @Override
     public void onEnable() {
+        loadConfig();
         getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    private void loadConfig() {
+        FileConfiguration config = getConfig();
+        config.addDefault(path_gameModeSurvivalWhenJoin, gameModeSurvivalWhenJoin);
+        gameModeSurvivalWhenJoin = config.getBoolean(path_gameModeSurvivalWhenJoin);
+        saveDefaultConfig();
     }
 
     @EventHandler
@@ -23,6 +36,14 @@ public class SpectatorModeTweaks extends JavaPlugin implements Listener {
         ) return;
 
         event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if (!gameModeSurvivalWhenJoin) return;
+        final GameMode gameMode = event.getPlayer().getGameMode();
+        if (gameMode == GameMode.SURVIVAL) return;
+        event.getPlayer().setGameMode(GameMode.SURVIVAL);
     }
 
 }
